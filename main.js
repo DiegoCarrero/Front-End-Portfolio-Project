daa8c3f50d8a588f78e4414e7b297274ac5d19f7
Diegocarrero <diegocar2413@gmail.com>
2023-01-28 16:48:25 -0500

    Fetch data from API and populate dropdown box

--- main.js
+++ main.js
@@ -1,5 +1,37 @@
+// API URL
+const API_URL = 'https://swapi.dev/api/planets/'
+
+// Selects planets dropdown box
+const swPlanets = document.getElementById('planets-dropdown');
+
+// Populates the dropdown with planet names
+fetch(API_URL)
+    .then(result => result.json())
+    .then((planets) => {
+        console.log(planets);
+        // Creates an option in the dropdown for each planet name
+        for (let i = 0; i < planets.results.length; i++) {
+            const option = document.createElement('option');
+            option.value = i + 1;
+            option.name = planets.results.name;
+            option.innerText = planets.results[i].name;
+            swPlanets.append(option);
+        }
+        fetch(planets.next).then(res => res.json()).then((morePlanets) => {
+            for (let i = 0; i < morePlanets.results.length; i++) {
+                const option = document.createElement('option');
+                option.value = i + 11;
+                option.name = morePlanets.results.name;
+                option.innerText = morePlanets.results[i].name;
+                swPlanets.append(option);
+            }
+        })
+        .catch(console.error);
+    })
+.catch(console.error);
 
 
-function populate() {
-    
-}
+// Fetches information about the selected planet
+// function getInfo(planetName) {
+
+// }
